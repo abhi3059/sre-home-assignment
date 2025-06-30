ff088b187fdb778c44f984bbb5ae91e839637afc
Abhinav Prasad <abhprasa@akamai.com>
2025-06-30 15:18:08 +0530

    redis

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,4 +1,4 @@
-import aioredis
+import redis.asyncio as redis
 import os
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
@@ -6,7 +6,7 @@ REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_TTL = 300
 
 async def connect_to_redis():
-    return await aioredis.from_url(
+    return await redis.from_url(
         f"redis://{REDIS_HOST}:{REDIS_PORT}",
         encoding="utf8",
         decode_responses=True
