f529abfc10d286e1b81edaae6753827a91d139fb
Abhinav Prasad <abhprasa@akamai.com>
2025-06-30 15:23:25 +0530

    redis

--- app/redis_client.py
+++ app/redis_client.py
@@ -5,9 +5,9 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_TTL = 300
 
-async def connect_to_redis():
-    return await redis.from_url(
+def connect_to_redis():
+    return redis.Redis.from_url(
         f"redis://{REDIS_HOST}:{REDIS_PORT}",
-        encoding="utf8",
+        encoding="utf-8",
         decode_responses=True
     )
