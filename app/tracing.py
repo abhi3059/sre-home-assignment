69f342cfe46788e551b84bfcdc834cce83868d3a
Abhinav Prasad <abhprasa@akamai.com>
2025-07-03 20:15:02 +0530

    modified tracing.py  files

--- app/tracing.py
+++ app/tracing.py
@@ -1,10 +1,10 @@
-import os
-from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
+import os
 
 def setup_tracer(app):
     trace.set_tracer_provider(
@@ -12,12 +12,13 @@ def setup_tracer(app):
             resource=Resource.create({SERVICE_NAME: "rickmorty-api"})
         )
     )
-    
+
     otlp_endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4318")
 
     otlp_exporter = OTLPSpanExporter(
-        endpoint=f"{otlp_endpoint}/v1/traces"
+        endpoint=otlp_endpoint  
     )
 
-    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
-    FastAPIInstrumentor.instrument_app(app)
+    span_processor = BatchSpanProcessor(otlp_exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
+    FastAPIInstrumentor().instrument_app(app)
